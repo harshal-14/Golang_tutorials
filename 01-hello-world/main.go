591be4d62b52faf15e17c97a89cdb109af95b9f4
Lucifer <harshalbhat14@gmail.com>
2024-05-07 23:13:32 -0400

    Hello Golang!

---
+++ 01-hello-world/main.go
@@ -0,0 +1,10 @@
+// Declares which file this program belongs to
+package main
+
+// This package is used to print to the console
+import "fmt"
+
+func main() {
+	// This will print "Hello, World!" to the console
+	fmt.Println("Hello, World! This is my first Go program!")
+}
